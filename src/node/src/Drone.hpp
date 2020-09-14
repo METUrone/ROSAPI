7dc3f81414cc3dd091b3230f12bc3c768f9f711f
alpylmz <ylmz.alp.e@gmail.com>
2020-09-14 17:57:20 +0300

    definitions of Drone class

--- src/node/src/Drone.hpp
+++ src/node/src/Drone.hpp
@@ -5,9 +5,22 @@ class Drone{
     public:
         Drone();
         ~Drone();
+        
+        void move(long double x, long double y, long double z, long double t);
+        
+        bool is_armed();
+        
+        void arm();
+
+        // Lands exactly to z=0 position
+        void land();
+
+        // Lands a different position
+        void land(long double x, long double y, long double t);
+
 
     private:
         long double _posx, _posy, _posz; //These positions are likely to be changed by another data type
-        void move(long double x, long double y, long double z, long double t);
+        
 
 };
