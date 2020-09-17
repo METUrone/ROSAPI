10c18354fac5be19d4444e318a422826997c9f18
alpylmz <ylmz.alp.e@gmail.com>
2020-09-17 15:02:43 +0300

    First tests of library

--- src/node/src/Drone.cpp
+++ src/node/src/Drone.cpp
@@ -3,13 +3,13 @@
 //Take coordinates from GPS, and put them into our coordinates, x,y,z
 Drone::Drone(ros::NodeHandle nh){
     _nh = nh;
-    _state_sub = nh.subscribe<mavros_msgs::State>("mavros/state",10,state_cb);
+    _state_sub = nh.subscribe<mavros_msgs::State>("mavros/state",10,&Drone::state_cb,this); // https://answers.ros.org/question/282259/ros-class-with-callback-methods/
     _arming_client = _nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
     _set_mode_client = _nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
     ros::Rate rate(20.0);
     while(ros::ok() && !_current_state.connected){
-        ros::SpinOnce();
+        ros::spinOnce();
         rate.sleep();
     }
 }
@@ -29,7 +29,7 @@ void Drone::move_relative(long double x, long double y, long double z, long doub
     pose.pose.position.z = _posz + z;
 
     local_pos_pub.publish(pose);
-    ros::SpinOnce();
+    ros::spinOnce();
 }
 
 void Drone::move_global(long double x, long double y, long double z, long double t){
@@ -47,8 +47,38 @@ void Drone::move_global(long double x, long double y, long double z, long double
     pose.pose.position.z = z;
 
     local_pos_pub.publish(pose);
-    ros::SpinOnce();
+    ros::spinOnce();
 }
 void Drone::state_cb(const mavros_msgs::State::ConstPtr& msg){
     _current_state = *msg;
+}
+
+bool Drone::is_armed(){
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    ros::Time last_request = ros::Time::now();
+    while(ros::ok()){
+        if( _current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( _set_mode_client.call(offb_set_mode) &&
+                    offb_set_mode.response.mode_sent)
+                        ROS_INFO("Offboard enabled");
+            last_request = ros::Time::now();
+            }
+        else{
+            if( !_current_state.armed && 
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                    if( _arming_client.call(arm_cmd) &&
+                        arm_cmd.response.success){
+                            ROS_INFO("Vehicle armed");
+                            return true;
+                        }
+                }
+        }
+    }
+    return false;//why ?
+
 }
---
+++ src/node/src/node1.cpp
@@ -0,0 +1,8 @@
+#include <Drone.hpp>
+
+int main(int argc, char **argv){
+    ros::init(argc,argv,"node1");
+    ros::NodeHandle nh;
+    Drone dron(nh);
+    dron.move_relative(2,0,0,0);
+}
