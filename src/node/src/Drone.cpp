21ab3347407773caf996143713fb6f9eeef5a1ec
alpylmz <ylmz.alp.e@gmail.com>
2020-09-17 11:58:42 +0300

    First steps for move

--- src/node/src/Drone.cpp
+++ src/node/src/Drone.cpp
@@ -1,12 +1,20 @@
 #include "Drone.hpp"
 
 //Take coordinates from GPS, and put them into our coordinates, x,y,z
-Drone::Drone(){
-    ros::NodeHandle nh;
-    ;
+Drone::Drone(ros::NodeHandle nh){
+    _nh = nh;
+    _state_sub = nh.subscribe<mavros_msgs::State>("mavros/state",10,state_cb);
+    _arming_client = _nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
+    _set_mode_client = _nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
+
+    ros::Rate rate(20.0);
+    while(ros::ok() && !_current_state.connected){
+        ros::SpinOnce();
+        rate.sleep();
+    }
 }
 
-void Drone::move(long double x, long double y, long double z, long double t){
+void Drone::move_relative(long double x, long double y, long double z, long double t){
     //When to rotate? At the start of the move, while move, or at the end of the move?
 
     //Need to check if armed
@@ -14,7 +22,7 @@ void Drone::move(long double x, long double y, long double z, long double t){
         arm();
     }
 
-    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",10);
+    ros::Publisher local_pos_pub = _nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",10);
     geometry_msgs::PoseStamped pose;
     pose.pose.position.x = _posx + x;
     pose.pose.position.y = _posy + y;
@@ -22,4 +30,25 @@ void Drone::move(long double x, long double y, long double z, long double t){
 
     local_pos_pub.publish(pose);
     ros::SpinOnce();
+}
+
+void Drone::move_global(long double x, long double y, long double z, long double t){
+    //When to rotate? At the start of the move, while move, or at the end of the move?
+
+    //Need to check if armed
+    if(!is_armed()){
+        arm();
+    }
+
+    ros::Publisher local_pos_pub = _nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",10);
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = x;
+    pose.pose.position.y = y;
+    pose.pose.position.z = z;
+
+    local_pos_pub.publish(pose);
+    ros::SpinOnce();
+}
+void Drone::state_cb(const mavros_msgs::State::ConstPtr& msg){
+    _current_state = *msg;
 }
--- src/node/src/Drone.hpp
+++ src/node/src/Drone.hpp
@@ -7,10 +7,13 @@
 
 class Drone{
     public:
-        Drone();
+        Drone(ros::NodeHandle nh);
         ~Drone();
         
-        void move(long double x, long double y, long double z, long double t);
+        void move_relative(long double x, long double y, long double z, long double t);
+
+        //Other than the other move function, this fuction takes the exact coordinates to move
+        void move_global(long double x, long double y, long double z, long double t);
 
         //Here x,y,z gives us the center of the circle, r is radius, t is the same as the t in move function.
         void circular_move(long double x, long double y, long double z, long double t, long double r);
@@ -28,6 +31,9 @@ class Drone{
 
     private:
         long double _posx, _posy, _posz; //These positions are likely to be changed by another data type
-        ros::NodeHandle nh; //?
-
+        ros::NodeHandle _nh; //?
+        mavros_msgs::State _current_state;
+        ros::Subscriber _state_sub;
+        ros::ServiceClient _arming_client, _set_mode_client;
+        void state_cb(const mavros_msgs::State::ConstPtr& msg);
 };
